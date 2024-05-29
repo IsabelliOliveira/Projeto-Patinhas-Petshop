749a51a4055e75818dd1f15157b21dcc23da5678
Isabelli Oliveira <104120168+IsabelliOliveira@users.noreply.github.com>
2024-05-28 23:25:40 -0300

    alteração criando dash

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        if (section.id === sectionId) {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
+    });
+}
--- patinhas.sql
+++ patinhas.sql
@@ -22,6 +22,9 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
+-- Copiando estrutura do banco de dados para bd_patinhas
+CREATE DATABASE IF NOT EXISTS `patinhas` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `patinhas`;
 
 --
 -- Estrutura para tabela `agendamento_servico`
